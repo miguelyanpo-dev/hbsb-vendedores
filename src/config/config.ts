2e335089822c5b90f74204b57aea8648fa24107c
miguelyanpo-dev <miguel.yanpo@gmail.com>
2026-04-17 09:37:44 -0500

    add-changes

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   port: Number(process.env.PORT || 3001),
   env: process.env.NODE_ENV || 'development',
-  productionUrl: process.env.PRODUCTION_URL || 'https://hbsb-limpio.vercel.app',
+  productionUrl: process.env.PRODUCTION_URL || 'https://hbsb-vendedores.vercel.app',
   cors: {
     origins: process.env.CORS_ORIGIN?.split(',').map(o => o.trim()) || ['*']
   }
